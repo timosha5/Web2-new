aceb5523b6d5735188d7c4e3510b0af7c51b06ae
timosha5 <107199949+timosha5@users.noreply.github.com>
2022-08-11 21:25:47 +0300

    part 8

--- js/script.js
+++ js/script.js
@@ -6,22 +6,31 @@ let header = $("#header");
 let intro = $("#intro"); 
 let introH = intro.innerHeight();
 let scrollPos = $(window).scrollTop();
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+
+checkScroll(scrollPos, introH)
 
 
-$(window).on("scroll load resize", function() {
+$(window).on("scroll resize", function() {
 	introH = intro.innerHeight();
     scrollPos = $(this).scrollTop();
 
+      checkScroll(scrollPos, introH);
+     
 
-     if(scrollPos > introH )  {
+    });
+
+      function checkScroll(scrollPos, introH) {
+      	if(scrollPos > introH )  {
      	header.addClass("fixed");
-
-
      } else {
       header.removeClass("fixed");
      }
 
-    });
+      }
+
+
 
 /*Smooth scroll*/
 
@@ -31,14 +40,36 @@ $("[data-scroll]").on("click", function(event){
 	let elementId = $(this).data('scroll');
 	let elementOffset = $(elementId).offset().top;
 
+	nav.removeClass("show");
+
 
 	$("html, body").animate({
-		scrollTop: elementOffset
+		scrollTop: elementOffset - 70
 
-	});
+	}, 700);
 
 });
 
+/* navToggle */
 
+
+navToggle.on(click, function(event) {
+	event.preventDefault();
+	nav.toggleClass("show");
+
+});
+
+/* Reviews*/
+
+let slider = $("reviewsSlider");
+
+slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+  fade: true,
+  arrows: false,
+  dors: true
+});
 
 });
